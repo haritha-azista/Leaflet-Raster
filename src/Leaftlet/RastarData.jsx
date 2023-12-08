f9bdfb4fbed97a7e5159e9a89f906e517f7726a1
haritha-azista <haritha.chintha@azistaindustries.com>
2023-12-08 18:27:46 +0530

    my first commit

--- src/Leaftlet/RastarData.jsx
+++ src/Leaftlet/RastarData.jsx
@@ -3,18 +3,17 @@ import { MapContainer, TileLayer, ImageOverlay } from 'react-leaflet';
 import img from './Screenshot 2023-12-08 173242.png'
 
 const RastarData = () => {
-  const mapCenter = [51.505, -0.09]; // Example center coordinates
-  const mapZoom = 13; // Example zoom level
+  const mapCenter = [51.505, -0.09];
+  const mapZoom = 13;
 
-  // Specify the image URL and bounds for the overlay
-  // const imageUrl ="https://i.imgur.com/Ion6X7C.jpg";
+  
   const imageUrl = img;
   const imageBounds = [
-    [17.444348, 78.385912],
-   [17.444542, 78.402357   ],
-   [17.450457, 78.385959   ],
-   [17.450784, 78.402529   ]
-  ]; // Example bounds
+   [17.444348, 78.385912],
+   [17.444542, 78.402357],
+   [17.450457, 78.385959],
+   [17.450784, 78.402529]
+  ]; 
 
   return (
     <MapContainer center={mapCenter} zoom={mapZoom} style={{ height: '500px', width: '100%' }}>
@@ -22,8 +21,6 @@ const RastarData = () => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-
-      {/* Add ImageOverlay component with image URL and bounds */}
       <ImageOverlay url={imageUrl} bounds={imageBounds} />
     </MapContainer>
   );
